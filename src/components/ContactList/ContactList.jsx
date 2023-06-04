ecfdd8e6013c65779af540aa4508d66869a49a0e
Serhii Halaka <serg.galaka@gmail.com>
2023-06-04 13:32:27 +0300

    no warnings

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { ContactListStyle } from './ContactList.styled';
-import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/contacts/contactSelectors';
 
 import ContactItem from 'components/ContactItem/ContactItem';
 
