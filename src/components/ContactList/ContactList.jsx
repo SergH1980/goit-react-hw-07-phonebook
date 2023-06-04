cb8dcfdb4111ec58e03ef0ca991eab3f0c1405cc
Serhii Halaka <serg.galaka@gmail.com>
2023-06-04 13:30:25 +0300

    fixed warnings

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { ContactListStyle, EmptyList } from './ContactList.styled';
+import { ContactListStyle } from './ContactList.styled';
 import { useSelector } from 'react-redux';
 import { getContacts } from 'redux/contacts/contactSelectors';
 
 import ContactItem from 'components/ContactItem/ContactItem';
 
 export default function ContactList() {
-  const contacts = useSelector(getContacts);
-
   return (
     <ContactListStyle>
       <ContactItem />
