2b6d9e4a73b3295c2e461e32c89343b3b476a3dd
Serhii Halaka <serg.galaka@gmail.com>
2023-06-05 23:34:23 +0300

    Loader on List sending multiple requests

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { ContactListStyle } from './ContactList.styled';
-// import { selectError, selectOperation } from 'redux/contacts/contactSelectors';
+import { selectError, selectOperation } from 'redux/contacts/contactSelectors';
 
 import ContactItem from 'components/ContactItem/ContactItem';
 
 export default function ContactList() {
-  // const error = useSelector(selectError);
-  // const operation = useSelector(selectOperation);
+  const error = useSelector(selectError);
+  const operation = useSelector(selectOperation);
 
   return (
     <ContactListStyle>
-      {/* {operation === 'fetch' && !error ? (
+      {operation === 'fetch' && !error ? (
         <div>Loading. Please wait</div>
       ) : (
         <ContactItem />
-      )} */}
-      <ContactItem />
+      )}
+      {/* <ContactItem /> */}
     </ContactListStyle>
   );
 }
